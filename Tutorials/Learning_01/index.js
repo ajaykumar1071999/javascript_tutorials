9385a5948fa5a4b51e78d6f0a6de97e23309c9de
Ajay Kumar <ajaykumar1071999@gmail.com>
2024-03-02 14:27:19 +0530

    01_tutorials Added

--- Tutorials/Learning_01/index.js
+++ Tutorials/Learning_01/index.js
@@ -1,1 +1,26 @@
-console.log('Hello Ajay')
+// How to declare variables in JS 
+
+// How to declare constant variable in JS
+
+// declare a constant variable
+const accountNumber=5643565433;
+let accountEmail="ajaykumar@gmail.com";
+var accountPassword="Ajay@123";
+accountCity="Aligarh";
+let accountState;
+var accountStates;
+
+// try to re-assign value to constant variable
+
+// accountNumber=7758493929383;// can not re-assign value to constant variable
+
+// console.log(accountNumber);
+
+//Error will be show TypeError: Assignment to constant variable.
+
+// try to modify let,var variable
+
+accountEmail="ajaykumar101999@gmail.com";
+accountPassword="Nic@123";
+
+console.table([accountNumber,accountEmail,accountPassword,accountCity,accountState,accountStates])
