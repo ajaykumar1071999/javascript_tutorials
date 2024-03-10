198075288495595b8af27c3165fd82350c348824
Ajay Kumar <ajaykumar1071999@gmail.com>
2024-03-10 10:38:36 +0530

    number study

---
+++ Tutorials/Learning_01/03_number.js
@@ -0,0 +1,16 @@
+const score = 400;
+const balance = new Number(1000);
+console.log(balance.toString());
+console.log(score.toFixed(2));
+
+const otherNumber = 1130.1101;
+console.log(otherNumber.toPrecision(4))
+
+const humanRed = 100000;
+console.log(humanRed.toLocaleString('en-IN'))
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.9));
+console.log(Math.random() * 10)
